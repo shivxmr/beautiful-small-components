d971a502bdfe294ce9c235e9e2fcc726f69f0bca
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-06-08 14:50:18 +0530

    second component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import OrderButton from "./components/complete-order-button/OrderButton";
+import GameCard from "./components/game-card-hover-effect/GameCard";
 
 function App() {
   return (
     <div className="App">
-      <OrderButton />
+      <h1>
+        1. <OrderButton />
+      </h1>
+      <h1>
+        2. <GameCard />
+      </h1>
     </div>
   );
 }
