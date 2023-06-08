bf78f5f27f0f91c77c147a331a1cd4648b20c54e
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-06-08 14:15:40 +0530

    fully-functional complete-order-button

---
+++ src/components/complete-order-button/OrderButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './OrderButton.css';
+
+class OrderButton extends React.Component {
+    handleClick = () => {
+        const button = document.querySelector('.order');
+
+        if (!button.classList.contains('animate')) {
+            button.classList.add('animate');
+            setTimeout(() => {
+                button.classList.remove('animate');
+            }, 10000);
+        }
+    };
+
+    render() {
+        return (
+            <button className="order" onClick={this.handleClick}>
+                <span className="default">Complete Order</span>
+                <span className="success">
+                    Order Placed
+                    <svg viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                    </svg>
+                </span>
+                <div className="box"></div>
+                <div className="truck">
+                    <div className="back"></div>
+                    <div className="front">
+                        <div className="window"></div>
+                    </div>
+                    <div className="light top"></div>
+                    <div className="light bottom"></div>
+                </div>
+                <div className="lines"></div>
+            </button>
+        );
+    }
+}
+
+export default OrderButton;
